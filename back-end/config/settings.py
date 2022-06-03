991cb35c8b0016cca78a9c882df4a28740f2696b
Parsa <parsa.aminpour@gmail.com>
2022-06-03 16:29:04 -0400

    adding middleware address

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'swap.middleware.IncludesBlackList',
 ]
 
 ROOT_URLCONF = 'config.urls'

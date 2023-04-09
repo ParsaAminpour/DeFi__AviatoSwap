33008b1445d2657e5fb14d73ba37630748f60e40
Parsa <parsa.aminpour@gmail.com>
2023-04-09 07:39:26 +0430

    just a test commit

--- AviatoContract/contracts/AviatoTokenContract.sol
+++ AviatoContract/contracts/AviatoTokenContract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract AviatoTokenContract is ERC20 , Ownable{
     address[] private address_list;
     mapping(address => bool) private minted;

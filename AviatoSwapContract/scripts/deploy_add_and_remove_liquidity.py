c47241b39ac5dce619252d13cea5d0f42c017b43
Parsa <parsa.aminpour@gmail.com>
2023-10-13 19:53:28 +0330

    add liquidity in two seperated rounds

--- AviatoSwapContract/scripts/deploy_add_and_remove_liquidity.py
+++ AviatoSwapContract/scripts/deploy_add_and_remove_liquidity.py
@@ -19,6 +19,7 @@ theme = Theme({
 console = Console(theme=theme)
 
 UNISWAP_V2_ROUTER01 = "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a"
+factory_addr = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
 
 def deploy(acc):
     print(f"The account is: {acc.address}\n ")
@@ -30,6 +31,7 @@ def deploy(acc):
         swap_task = progress.add_task("Deploying Swap...", total=2)
         approval_task = progress.add_task("Approving to swap...", total=2)
         add_liquidity_task = progress.add_task("Adding Liquidity...", total=1)
+        add_liquidity_round2_task = progress.add_task("Adding Liquidity (2nd round)...", total=3)
 
         while not progress.finished:
             token1 = TokenA.deploy({'from':acc})
@@ -60,35 +62,66 @@ def deploy(acc):
             progress.update(approval_task, completed=2)
             
 
-            ##### ADD LIQUIDITY #####
+            ##### ADD LIQUIDITY (R01) #####
             tx = swap._bothSideAddingLiquidity(
                 addr1, addr2, 1e20, 1e20, acc, block.timestamp + 10800, 
                 {'from':acc})
             tx.wait(1)
             progress.update(add_liquidity_task, completed=1)
 
-            
-    factory_addr = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-    pair_addr = interface.IUniswapV2Factory(factory_addr).getPair(token1.address, token2.address)
+            pair_addr = interface.IUniswapV2Factory(factory_addr).getPair(token1.address, token2.address)
+            pair_token = interface.IERC20(pair_addr)
+            res_round1 = interface.IUniswapV2Pair(pair_addr).getReserves()
+            lp_balance_round1 = interface.IUniswapV2Pair(pair_addr).balanceOf(acc)
 
-    pair_token = interface.IERC20(pair_addr)
 
-    res = interface.IUniswapV2Pair(pair_addr).getReserves()
-    lp_balance = interface.IUniswapV2Pair(pair_addr).balanceOf(acc)
+            ######## ADD LIQUIDITY (R02) ########
+            token1.approve(swap.address, 1e20, {'from':acc})
+            time.sleep(1)
+            progress.update(add_liquidity_round2_task, completed=1)
+
+            token2.approve(swap.address, 1e20, {'from':acc})
+            time.sleep(1)
+            progress.update(add_liquidity_round2_task, completed=2)
+
+            second_add = swap._bothSideAddingLiquidity(
+                addr1, addr2, 1e20, 1e20, acc, block.timestamp + 10800,
+                {'from':acc})
+            second_add.wait(1)
+            progress.update(add_liquidity_round2_task, completed=3)
+
+            res_round2 = interface.IUniswapV2Pair(pair_addr).getReserves()
+            lp_balance_round2 = interface.IUniswapV2Pair(pair_addr).balanceOf(acc)
+
 
     print(Panel.fit(f"""
-    The reserves amount for token1 and token2 are\n
+    The reserves amount for token1 and token2 in first round are\n
     [bold green]
-        Reserve1 : {res[0]} => {res[0].to('ether')} TokenA\n
-        Reserve2 : {res[1]} => {res[1].to('ether')} TokenB
+        Reserve1 : {res_round1[0]} => {res_round1[0].to('ether')} TokenA\n
+        Reserve2 : {res_round1[1]} => {res_round1[1].to('ether')} TokenB
     [/]\n
     The pair token address is: [purple]{pair_token.address}[/]\n
-    And the liquidity token balance for this liquidity pair is\n
+    And the liquidity token balance for this liquidity pair in first round is\n
     [bold green]
-        {lp_balance} => {lp_balance.to('ether')} Lp token
+        {lp_balance_round1} => {lp_balance_round1.to('ether')} Lp token
+    [/]\n\n
+
+    The reserves amount for token1 and token2 in second round are\n
+    [bold green]
+        Reserve1 : {res_round2[0]} => {res_round2[0].to('ether')} TokenA\n
+        Reserve2 : {res_round2[1]} => {res_round2[1].to('ether')} TokenB
+    [/]\n
+    And the liquidity token balance for this liquidity pair in second round is\n
+    [bold green]
+        {lp_balance_round2} => {lp_balance_round2.to('ether')} Lp token
     [/]
+    
     """, title="Liquidty Pool Data"))
     print("\n\n")
+
+
+
+
 
 
     ########### REMOVE LIQUIDITY ###########
@@ -97,15 +130,15 @@ def deploy(acc):
         remove_liquidity = progress.add_task("Removing Liquidity...", total=3)
 
         while not progress.finished:   
-            if res[0] * res[1]!= 0: progress.update(preparing, completed=1)
+            if res_round1[0] * res_round1[1]!= 0: progress.update(preparing, completed=1)
             time.sleep(.5)
-            if lp_balance != 0: progress.update(preparing, completed=2)
+            if lp_balance_round2 != 0: progress.update(preparing, completed=2)
             time.sleep(.5)
             if pair_token.address == pair_addr : progress.update(preparing, completed=3)
             time.sleep(.5)
 
             optimal_lp_amount_to_burn = swap._calculateAmountOfLpTokenForBurn(
-                1e19, 1e19, res[0], res[1], lp_balance
+                1e19, 1e19, res_round1[0], res_round1[1], lp_balance_round1
             )
             progress.update(remove_liquidity, completed=1)
             
